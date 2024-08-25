b2994c2c6ba5b6c4e0dc7e96dadec479b348615d
I-Robot-90 <57416952+I-Robot-90@users.noreply.github.com>
2024-08-25 01:02:15 -0500

    Add files via upload
    Disgusted with myself because I was idle this week, mainly because the sheer magnitude of this challenge discouraged me a bit. I am still in the process of cooking up functions for the respective pieces, but at least I can say I made black and white pieces distinguishable

--- main.cpp
+++ main.cpp
@@ -2,65 +2,81 @@
 using namespace std;
 
 
-
 class Pieces {
 public:
     char piece;
     int value;
 
+    int pawnMove(){
+
+    };
+
 };
-Pieces pawn;
-Pieces bishop;
-Pieces knight;
-Pieces rook;
-Pieces queen;
-Pieces king;
+
+
+Pieces whitePawn;
+Pieces whiteBishop;
+Pieces whiteKnight;
+Pieces whiteRook;
+Pieces whiteQueen;
+Pieces whiteKing;
+
+
+Pieces blackPawn;
+Pieces blackBishop;
+Pieces blackKnight;
+Pieces blackRook;
+Pieces blackQueen;
+Pieces blackKing;
 
 class Board {
 public:
     const static int rows = 8;
     const static int cols = 8;
     pair<char, int> grid[rows][cols]{};
-//    char piece = (new Pieces())->piece;
 
 
 public: Board()= default;
 
+
     int printBoard() {
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
-                grid[1][j].first = pawn.piece;
-                grid[6][j].first = pawn.piece;
+                grid[1][j].first = blackPawn.piece;
+                grid[6][j].first = whitePawn.piece;
 
-                grid[0][0].first= rook.piece;
-                grid[0][7].first = rook.piece;
-                grid[7][0].first= rook.piece;
-                grid[7][7].first= rook.piece;
+                grid[0][0].first= blackRook.piece;
+                grid[0][7].first = blackRook.piece;
 
-                grid[0][3].first = king.piece;
-                grid[7][3].first = king.piece;
+                grid[7][0].first= whiteRook.piece;
+                grid[7][7].first= whiteRook.piece;
 
-                grid[0][4].first = queen.piece;
-                grid[7][4].first = queen.piece;
+                grid[0][3].first = blackKing.piece;
+                grid[7][3].first = whiteKing.piece;
 
-                grid[0][1].first = knight.piece;
-                grid[0][6].first = knight.piece;
-                grid[7][1].first = knight.piece;
-                grid[7][6].first = knight.piece;
+                grid[0][4].first = blackQueen.piece;
+                grid[7][4].first = whiteQueen.piece;
 
-                grid[0][2].first = bishop.piece;
-                grid[0][5].first = bishop.piece;
-                grid[7][2].first = bishop.piece;
-                grid[7][5].first = bishop.piece;
+                grid[0][1].first = blackKnight.piece;
+                grid[0][6].first = blackKnight.piece;
+
+                grid[7][1].first = whiteKnight.piece;
+                grid[7][6].first = whiteKnight.piece;
+
+                grid[0][2].first = blackBishop.piece;
+                grid[0][5].first = blackBishop.piece;
+
+                grid[7][2].first = whiteBishop.piece;
+                grid[7][5].first = whiteBishop.piece;
 
                 cout << grid[i][j].first;
 
                 if (j < cols - 1)
-                    cout << " |     ";
+                    cout << " |      ";
             }
             cout << endl;
             if (i < rows - 1)
-                cout << "-------------------------------------------------------" << endl;
+                cout << "----------------------------------------------------------------" << endl;
         }
         return grid[rows - 1][cols - 1].second;
     }
@@ -69,24 +85,44 @@ public: Board()= default;
 };
 
 
-int main() {
-    pawn.value = 1;
-    pawn.piece = 'p';
+int main(){
+    whitePawn.value = 1;
+    whitePawn.piece = 'P';
 
-    bishop.value = 1;
-    bishop.piece = 'B';
+    blackPawn.value = 1;
+    blackPawn.piece = 'p';
 
-    knight.value = 1;
-    knight.piece = 'H';
+    whiteBishop.value = 3;
+    whiteBishop.piece = 'B';
 
-    rook.value = 1;
-    rook.piece = 'R';
+    blackBishop.value = 3;
+    blackBishop.piece = 'b';
 
-    queen.value = 1;
-    queen.piece = 'Q';
+    whiteKnight.value = 3;
+    whiteKnight.piece = 'H';
 
-    king.value = 0;
-    king.piece = 'K';
+    blackKnight.value = 3;
+    blackKnight.piece = 'h';
+
+    whiteRook.value = 5;
+    whiteRook.piece = 'R';
+
+    blackRook.value = 5;
+    blackRook.piece = 'r';
+
+    whiteQueen.value = 9;
+    whiteQueen.piece = 'Q';
+
+    blackQueen.value = 9;
+    blackQueen.piece = 'q';
+
+    whiteKing.value = 0;
+    whiteKing.piece = 'K';
+
+    blackKing.value = 0;
+    blackKing.piece = 'k';
+
+
     Board board;
     board.printBoard();
     return 0;
